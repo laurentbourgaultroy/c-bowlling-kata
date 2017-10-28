712460f35b38e0aa4586e683a385ccec55b3adb4
Laurent Bourgault-Roy <laurentbr@gmail.com>
2017-10-27 20:30:06 -0400

    small cleanups

--- bowling.c
+++ bowling.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "bowling.h"
 
+#define ALL_PINS 10
+
 int is_last_frame(int *rolls) {
   for (int i = 0; i < 4; i++)
   {
@@ -12,20 +14,21 @@ int is_last_frame(int *rolls) {
 
 int is_spare(int *rolls)
 {
-  if(rolls[0] + rolls[1] == 10) return 1;
+  if(rolls[0] + rolls[1] == ALL_PINS) return 1;
   return 0;
 }
 
 int is_strike(int *rolls)
 {
-  if (rolls[0] == 10) return 1;
+  if (rolls[0] == ALL_PINS) return 1;
   return 0;
 }
 
 int sum(int *rolls, int max)
 {
   int result = 0;
-  for (int i = 0; i < max; i++) {
+  for (int i = 0; i < max; i++)
+  {
     if (rolls[i] == NO_ROLL) return result;
     result += rolls[i];
   };
@@ -34,19 +37,9 @@ int sum(int *rolls, int max)
 
 int bowling_score(int *rolls)
 {
-  if (is_last_frame(rolls))
-  {
-    return sum(rolls, 3);
-  }
-
-  if (is_strike(rolls))
-  {
-    return sum(rolls, 3) + bowling_score(&rolls[1]);
-  }
-
-  if (is_spare(rolls)) {
-    return sum(rolls, 3) + bowling_score(&rolls[2]);
-  }
+  if (is_last_frame(rolls)) return sum(rolls, 3);
+  if (is_strike(rolls)) return sum(rolls, 3) + bowling_score(&rolls[1]);
+  if (is_spare(rolls)) return sum(rolls, 3) + bowling_score(&rolls[2]);
 
   return sum(rolls, 2) + bowling_score(&rolls[2]);
 }
